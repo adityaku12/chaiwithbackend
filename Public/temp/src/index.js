1d97a14a3863133591ba760ecde52b648f72609a
Aditya kumar <adityakumarbju121@gmail.com>
2026-01-09 22:45:46 +0530

    on a board

--- Public/temp/src/index.js
+++ Public/temp/src/index.js
@@ -1,11 +1,31 @@
-import mongoose from "mongoose";
+require('dotenv').config()
 
+import mongoose from "mongoose";
+import { DB_NAME } from "./costants";
+
+
+
+connectDB ()
+
+
+
+
+
+/*import  express from "express";
+const app = express();
 
 (async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=>{
+             console.error("Failed to connect to MongoDB: ", error);
+             throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
-})();
+})(); */
